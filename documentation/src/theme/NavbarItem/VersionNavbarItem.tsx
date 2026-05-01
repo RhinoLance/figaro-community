4c1c4277cce3a39b9cff4506b7391f323a09b052
Lance Conry <lance@rhinosw.com>
2026-05-01 14:47:29 +1000

    version number improvements

--- documentation/src/theme/NavbarItem/VersionNavbarItem.tsx
+++ documentation/src/theme/NavbarItem/VersionNavbarItem.tsx
@@ -3,7 +3,7 @@ import clsx from 'clsx';
 import pkg from '@site/package.json';
 
 export default function VersionNavbarItem(props: any): ReactNode {
-  const version = pkg.version;
+  const version = pkg.version.split('.').slice(0, 2).join('.');
 
   return (
     <div className={clsx('navbar-item', 'version-navbar-item')}
